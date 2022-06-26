34cd018e474f41a7ce23cd219c606bae61a62373
OmarAlCoptan <omarcoptan9@gmail.com>
2022-06-26 18:52:31 +0200

    lavender: PixelPlusUI Bringup
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -8,17 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Weeb Project stuff
-$(call inherit-product, vendor/weeb/build/product/weeb_product.mk)
+# Inherit some common PPUI stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+PIXEL_LAUNCHER_MOD := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
-WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := weeb_lavender
+PRODUCT_NAME := aosp_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
