40a688bdf58ac079d8d9ece372f3ae17a7471d2a
OmarAlCoptan <omarcoptan9@gmail.com>
2022-09-07 19:38:16 +0200

    lavender: add vendorsetup
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/OmarAlCoptan/vendor_xiaomi_lavender.git -b 12.1 vendor/xiaomi/lavender
+git clone https://github.com/projects-nexus/nexus_kernel_xiaomi_lavender.git -b qti kernel/xiaomi/lavender
+
